8d57e461c150e220d6a3db67519ac2b4b708d143
ale <alessandroterranova2@gamil.com>
2025-09-18 23:29:35 +0200

    step1_filter: aggiunti altri tagli di accettanza e costruiti i candidati Jspi aggiunto file utils.cpp con le funzioni definite come supporto

--- step1_filter.py
+++ step1_filter.py
@@ -1,10 +1,11 @@
 import os
 import ROOT
 
-ROOT.EnableImplicitMT()  # Abilita il multithreading implicito in ROOT
+# ROOT.EnableImplicitMT()  # Abilita il multithreading implicito in ROOT
 
 # Creo il percorso per la cartella contente i file
 path = os.path.dirname(__file__)
+
 
 
 def FilterCollection(rdf, collection, new_col=None, mask=None, indices=None):
@@ -27,7 +28,6 @@ def FilterCollection(rdf, collection, new_col=None, mask=None, indices=None):
     columns = rdf.GetColumnNames()
 
     redefine = True
-
     # Se new_col è specificato, controllo che non esistano già colonne con quel prefisso
     if new_col is not None:
         redefine = False
@@ -37,22 +37,22 @@ def FilterCollection(rdf, collection, new_col=None, mask=None, indices=None):
 
     # Funzione per definire o ridefinire una colonna in base alla sua esistenza nel DataFrame
     # name è il nome della colonna, expr è l'espressione (mask o indices)
-    def define_or_redefine(name, expr):
-        if name not in columns:
-            return rdf.Define(name, expr)
-        else:
+    def define_or_redefine(name, expr, redefine):
+        if redefine:
             return rdf.Redefine(name, expr)
+        else:
+            return rdf.Define(name, expr)
 
     # Controllo mask
     if mask is not None:
         mask_name = f"__mask__{collection}__{new_col}"
-        rdf = define_or_redefine(mask_name, mask)
+        rdf = define_or_redefine(mask_name, mask, mask_name in columns)
         mask = mask_name  # aggiorna il riferimento
 
     # Controllo indices
     if indices is not None:
         indices_name = f"__indices__{collection}__{new_col}"
-        rdf = define_or_redefine(indices_name, indices)
+        rdf = define_or_redefine(indices_name, indices, indices_name in columns)
         indices = indices_name  # aggiorna il riferimento
 
     # Creo la lista delle variabili da filtrare senza includere quelle in exclude
@@ -62,20 +62,13 @@ def FilterCollection(rdf, collection, new_col=None, mask=None, indices=None):
         if name.startswith(collection + "_") and name not in exclude
     ]
 
-    # Funzione per definire o ridefinire una colonna in base al flag redefine
-    def RedefineOrDefine(name, expr):
-        if redefine:
-            return rdf.Redefine(name, expr)
-        else:
-            return rdf.Define(name, expr)
-
     # Applico il filtro a tutte le variabili della collezione
     for var in vars:
         target = var if redefine else var.replace(collection, new_col)
         if mask:
-            rdf = RedefineOrDefine(target, f"{var}[{mask}]")
+            rdf = define_or_redefine(target, f"{var}[{mask}]", redefine)
         if indices:
-            rdf = RedefineOrDefine(target, f"Take({var}, {indices})")
+            rdf = define_or_redefine(target, f"Take({var}, {indices})", redefine)
 
     # Definisco o ridefinisco la variabile che conta il numero di elementi nella collezione filtrata
     if mask:
@@ -84,11 +77,9 @@ def FilterCollection(rdf, collection, new_col=None, mask=None, indices=None):
         size = f"{indices}.size()"
 
     size_name = f"n{collection}" if redefine else f"n{new_col}"
-    RedefineOrDefine(size_name, size)
+    rdf = define_or_redefine(size_name, size, redefine)
 
     return rdf
-
-
 
 if __name__ == "__main__":
 
@@ -137,6 +128,11 @@ if __name__ == "__main__":
     rdf = FilterCollection(rdf, "Muon", new_col="Dimu_m1", indices="Dimu_t1muIdx")
     rdf = FilterCollection(rdf, "Muon", new_col="Dimu_m2", indices="Dimu_t2muIdx")
 
+    # Definisco la probabilità del vertice del dimuon ricostruendo il vertice della J/psi
+    rdf = rdf.Define("Dimu_vertexProb", "ROOT::VecOps::Map(Dimu_chi2, getProb)")
+
+    rdf = rdf.Define("Dimu_rapidity", "rapidity(Dimu_pt, Dimu_eta)")  # Definisco la rapidità del dimuon
+
     # Filtro i dimuon in base a vari criteri di qualità
     rdf = FilterCollection(
         rdf,
@@ -149,7 +145,23 @@ if __name__ == "__main__":
             Dimu_m1_dz < 30 && Dimu_m2_dz < 30 &&
             Dimu_m1_Chi2 < 1.8 && Dimu_m2_Chi2 < 1.8 &&
             (Dimu_m1_isInTightAccept || Dimu_m1_isInLooseAccept) && (Dimu_m2_isInTightAccept || Dimu_m2_isInLooseAccept) &&
-            Dimu_mass > 2.8 && Dimu_mass < 3.35
+            Dimu_mass > 2.8 && Dimu_mass < 3.35 &&
+            Dimu_vertexProb > 0.005 &&
+            JpsiAcceptance(Dimu_pt, Dimu_rapidity)
             """,
     ).Filter("nDimu >= 2")
+
+    rdf = rdf.Define("Dimu_CandidateIdx", "JpsiCandidates(Dimu_vertexProb, Dimu_t1muIdx, Dimu_t2muIdx)")  # Indici dei dimuon candidati J/psi
+    #Sommo i candidati J/psi + 1 così i non candidati sono 0, e i candidati sono 1 o 2
+    rdf = rdf.Filter("Sum(Dimu_CandidateIdx + 1) == 3")  # Filtro gli eventi con esattamente due candidati J/psi
+
+    rdf = FilterCollection(rdf, "Dimu", new_col="Jpsi", mask = "Dimu_CandidateIdx != -1")  # Creo collezione dei dimuon candidati J/psi
+    
+    rdf = FilterCollection(rdf, "Jpsi", indices = "ROOT::VecOps::Argsort(Jpsi_CandidateIdx)")  # Ordino i dimuon J/psi in base agli indici dei candidati
+    rdf = rdf.Filter("""
+                     Jpsi_dl[0] > -0.05 && Jpsi_dl[0] < 0.1
+                     """)
+ 
+ 
+    rdf.Display(["Jpsi_mass", "Jpsi_CandidateIdx", "Jpsi_t1muIdx", "Jpsi_t2muIdx", "Jpsi_vertexProb" ]).Print()  # Stampo le variabili del DataFrame
 
---
+++ utils.cpp
@@ -0,0 +1,129 @@
+#ifndef UTILS_CPP
+#define UTILS_CPP
+#include "TMath.h"
+#include "ROOT/RVec.hxx"
+
+
+ROOT::VecOps::RVec<bool> dRpTMatch(const ROOT::VecOps::RVec<float> &eta1,
+                                   const ROOT::VecOps::RVec<float> &phi1,
+                                   const ROOT::VecOps::RVec<float> &pt1,
+                                   const ROOT::VecOps::RVec<float> &eta2,
+                                   const ROOT::VecOps::RVec<float> &phi2,
+                                   const ROOT::VecOps::RVec<float> &pt2,
+                                   float dRmax = 0.1, float dPtMax = 10) {
+  ROOT::VecOps::RVec<bool> matches(eta1.size(), false);
+  for (size_t i = 0; i < eta1.size(); ++i)
+    for (size_t j = 0; j < eta2.size(); ++j)
+      if (ROOT::VecOps::DeltaR(eta1[i], eta2[j], phi1[i], phi2[j]) < dRmax &&
+          fabs(pt1[i] - pt2[j]) < dPtMax) {
+        matches[i] = true;
+        break;
+      };
+  return matches;
+};
+
+ROOT::VecOps::RVec<bool>
+tightMuAcceptance(const ROOT::VecOps::RVec<bool> &isTrigMatched,
+                  const ROOT::VecOps::RVec<float> &pt,
+                  const ROOT::VecOps::RVec<float> &eta) {
+  ROOT::VecOps::RVec<bool> acceptances(pt.size(), false);
+  for (size_t i = 0; i < pt.size(); ++i) {
+    if (!isTrigMatched[i]) {
+      continue;
+    }
+
+    if ((fabs(eta[i]) < 1.2 && pt[i] > 3.5) ||
+        (fabs(eta[i]) > 1.2 && eta[i] < 1.6 &&
+         pt[i] > -15. * fabs(eta[i]) / 4 + 8) ||
+        (fabs(eta[i]) > 1.6 && fabs(eta[i]) < 2.4 && pt[i] > 3.5)) {
+      acceptances[i] = true;
+    }
+  }
+  return acceptances;
+}
+
+ROOT::VecOps::RVec<bool>
+looseMuAcceptance(const ROOT::VecOps::RVec<float> &pt,
+                  const ROOT::VecOps::RVec<float> &eta) {
+  ROOT::VecOps::RVec<bool> acceptances(pt.size(), false);
+  for (size_t i = 0; i < pt.size(); ++i) {
+    if (fabs(eta[i]) < 1.2 && pt[i] > 3 ||
+        fabs(eta[i]) >= 1.2 && fabs(eta[i]) < 2.4 &&
+            sqrt(pt[i] * pt[i] + pt[i] * sinh(fabs(eta[i]))) > 3) {
+      acceptances[i] = true;
+    }
+  }
+  return acceptances;
+}
+
+bool MuonsAcceptance(const ROOT::VecOps::RVec<bool> &isInTightAccept,
+                     const ROOT::VecOps::RVec<bool> &isInLooseAccept) {
+  
+  if (Sum(isInTightAccept) >= 4 and Sum(isInLooseAccept) >= 1) { // caso in cui tutti e quattro i muoni sono in accettanza tight
+    return true;
+  } else if (Sum(isInTightAccept) == 3 and
+             Sum(!isInTightAccept && isInLooseAccept) >= 1) { // caso in cui tre muoni sono in accettanza tight e uno in accettanza loose, ma non tight
+    return true;
+    } else {
+    return false;
+      }
+}
+
+// Lambda function per calcolare la probabilità associata a un valore di chi2 con 1 grado di libertà
+auto getProb = [](float chi2){return TMath::Prob(chi2,1);};
+
+// Funzione per la rapidità della J/psi
+ROOT::VecOps::RVec<float>
+rapidity(const ROOT::VecOps::RVec<float> &pt, const ROOT::VecOps::RVec<float> &eta) {
+  ROOT::VecOps::RVec<float> y(pt.size());
+  for ( int i = 0; i < pt.size(); ++i) {
+    float pz = pt[i] * sinh(eta[i]);
+    float energy = sqrt(pt[i] * pt[i] + pz*pz + 0.1056580 * 0.1056580);
+    y[i] = 0.5 * log((energy + pz) / (energy - pz));
+  }
+  return y;
+}
+
+// Funzone per l'accettanza della J/psi
+ROOT::VecOps::RVec<bool>
+JpsiAcceptance(const ROOT::VecOps::RVec<float> &pt, const ROOT::VecOps::RVec<float> &y) {
+  ROOT::VecOps::RVec<bool> acceptances(pt.size(), false);
+  for (size_t i = 0; i < pt.size(); ++i) {
+    if ((fabs(y[i]) < 1.2 &&  pt[i] > 6.5) ||
+        (fabs(y[i]) >= 1.2 && fabs(y[i]) < 1.43 &&
+         pt[i] > -200 * fabs(y[i])/23  + 779/46) ||
+        (fabs(y[i]) >= 1.43 && fabs(y[i]) < 2.2 && pt[i] > 4.5)) {
+      acceptances[i] = true;
+    }
+  }
+  return acceptances;
+}
+
+// Funzione che resituisce un Rvecinteger con gli indici degli elementi candidati a formare la coppia di J/psi
+// La funzione prende in input il vettore delle probabilità dei vertici, il vettore dei firstindex e il vettore dei secondindex
+// Restituisce un Rvecinteger con gli indici dei due dimuon candidati a formare la coppia di J/psi
+ROOT::VecOps::RVec<int> JpsiCandidates(ROOT::VecOps::RVec<float> Vtxprob, ROOT::VecOps::RVec<int> firstindex, ROOT::VecOps::RVec<int> secondindex) {
+  int nDimu = Vtxprob.size();
+  ROOT::VecOps::RVec<int> CandidateIndexs(nDimu, -1); // Inizializzo il vettore degli indici dei candidati a -1
+  ROOT::VecOps::RVec<int> SortedIndexs = ROOT::VecOps::Argsort(-Vtxprob); // Ordino gli indici in modo decrescente in base alla probabilità del vertice
+  // ROOT::VecOps::RVec<float> VertexProbSorted = ROOT::VecOps::Take(Vtxprob, SortedIndexs); // Creo un vettore con le probabilità ordinate
+  ROOT::VecOps::RVec<int> FirstIndexSorted = ROOT::VecOps::Take(firstindex, SortedIndexs); // Creo un vettore con i firstindex ordinati
+  ROOT::VecOps::RVec<int> SecondIndexSorted = ROOT::VecOps::Take(secondindex, SortedIndexs); // Creo un vettore con i secondindex ordinati
+  for(int i1 = 0; i1 < nDimu; ++i1) {
+    for (int i2 = i1+1; i2 < nDimu; ++i2) { // Non confronto mai un dimuon con se stesso
+      if (FirstIndexSorted[i1] != FirstIndexSorted[i2] && FirstIndexSorted[i1] != SecondIndexSorted[i2] && 
+          SecondIndexSorted[i1] != FirstIndexSorted[i2] && SecondIndexSorted[i1] != SecondIndexSorted[i2]) {
+        // Se i due dimuon condividono almeno un muone, esco dal ciclo interno
+        CandidateIndexs[SortedIndexs[i1]] = 0; // Segno il dimuon come candidato
+        CandidateIndexs[SortedIndexs[i2]] = 1; // Segno il dimuon come candidato
+        return CandidateIndexs; // Esco dalla funzione restituendo gli indici dei candidati
+      }
+    }
+  }
+
+
+  
+  return CandidateIndexs;
+}
+
+#endif // UTILS_CPP
